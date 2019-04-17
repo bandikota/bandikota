0049425ab4cbf774373d106f35aedb6f3dfdbe7b
bandikota <bandikota@mail.ru>
2019-04-17 19:43:06 +0500

    Add files via upload

---
+++ precache-manifest.6de79fb1f18b0e41baacf994489a2c67.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e0dfb76136a217dfa0decf7c1cc86aca",
+    "url": "/static/media/logo.e0dfb761.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0450d0876e6f9e940f69",
+    "url": "/static/js/main.f50f491f.chunk.js"
+  },
+  {
+    "revision": "35afed8311a0333075c6",
+    "url": "/static/js/2.3835347d.chunk.js"
+  },
+  {
+    "revision": "0450d0876e6f9e940f69",
+    "url": "/static/css/main.282b5ae9.chunk.css"
+  },
+  {
+    "revision": "f98df23715bbb691646c3b00da1148b8",
+    "url": "/index.html"
+  }
+];
