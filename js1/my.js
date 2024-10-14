57e97ed220d4d9bc06e157aa21b6670b51ab9c2b
bandikota <bandikota@mail.ru>
2024-10-14 15:36:35 +0500

    Add files via upload

---
+++ js1/my.js
@@ -0,0 +1,24 @@
+$(function() {
+    $(".change-btn").on("click", function() {
+        $('body').toggleClass('dark-theme');
+        $('body').toggleClass('light-theme');
+    });
+})
+$(function() {
+    let header = $('.header');
+    let hederHeight = header.height(); // вычисляем высоту шапки
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 0.1) {
+            header.addClass('header_fixed');
+            $('body').css({
+                'paddingTop': hederHeight + 'px' // делаем отступ у body, равный высоте шапки
+            });
+        } else {
+            header.removeClass('header_fixed');
+            $('body').css({
+                'paddingTop': 0 // удаляю отступ у body, равный высоте шапки
+            })
+        }
+    });
+});
